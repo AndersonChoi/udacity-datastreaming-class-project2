25cfaf7b542ccd42072a4a5aaafcae528ea5a030
최원영(WonYoung Choi)/Data Infrastructure팀/SKP <circlezero@sk.com>
2019-12-04 11:50:28 +0900

    submit files

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # udacity-datastreaming-class-project2
 SF Crime Data Project
+
+## Submit codes
+
+- kafka_server.py
+- producer_server.py
+- consumer_server.py
+- data_stream.py
+
+## Question?
+
+Q : How did changing values on the SparkSession property parameters affect the throughput and latency of the data?
+
+A : I checked `inputRowsPerSecond` and `processedRowsPerSecond` value.
+
+Q : What were the 2-3 most efficient SparkSession property key/value pairs? Through testing multiple variations on values, how can you tell these were the most optimal?
+
+A : I checked `inputRowsPerSecond` and `processedRowsPerSecond` . Finally, I got a best key/value pairs like below.
+
+```
+spark.streaming.kafka.maxRatePerPartition : 100
+spark.streaming.backpressure.enabled : true
+spark.default.parallelism : 100
+spark.sql.shuffle.partitions : 100
+```
+
+## Environment
+
+- Python 3.7
+- Spark 2.3.4
+- Kafka 0.10
+
+## How to run
+
+### Producer
+```
+python kafka_server.py
+```
+### Consumer
+```
+python consumer_server.py
+```
+### Data stream
+```
+spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.4 --master local[*] data_stream.py
+```
---
+++ data_stream.py
@@ -0,0 +1,116 @@
+import logging
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+import pyspark.sql.functions as psf
+
+schema = StructType([
+    StructField('crime_id', StringType()),
+    StructField('original_crime_type_name', StringType()),
+    StructField('report_date', StringType()),
+    StructField('call_date', StringType()),
+    StructField('offense_date', StringType()),
+    StructField('call_time', StringType()),
+    StructField('call_date_time', StringType()),
+    StructField('disposition', StringType()),
+    StructField('address', StringType()),
+    StructField('city', StringType()),
+    StructField('state', StringType()),
+    StructField('agency_id', StringType()),
+    StructField('address_type', StringType()),
+    StructField('common_location', StringType())
+])
+
+
+def run_spark_job(spark):
+
+    print('run spark job')
+
+    # Create Spark configurations with max offset of 200 per trigger
+    # set up correct bootstrap server and port
+    df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("subscribe", "department.call.service.log") \
+        .option("startingOffsets", "earliest") \
+        .option("maxOffsetsPerTrigger", "100") \
+        .option("maxRatePerPartition", "2") \
+        .option("stopGracefullyOnShutdown", "true") \
+        .load()
+
+    print('=== printSchema')
+    # Show schema for the incoming resources for checks
+    df.printSchema()
+
+    # Take only value and convert it to String
+    kafka_df = df.selectExpr("CAST(value AS STRING)")
+
+    service_table = kafka_df\
+        .select(psf.from_json(psf.col('value'), schema).alias("DF_TABLE"))\
+        .select("DF_TABLE.*")
+
+    service_table.printSchema()
+
+    print('=== distinct_table')
+    # select original_crime_type_name and disposition
+    distinct_table = service_table \
+        .select("original_crime_type_name", "disposition") \
+        .distinct()
+
+    distinct_table.printSchema()
+
+    # count the number of original crime type
+    agg_df = distinct_table \
+        .dropna() \
+        .select("original_crime_type_name") \
+        .groupby("original_crime_type_name") \
+        .agg({"original_crime_type_name" : "count"}) \
+        .orderBy("count(original_crime_type_name)", ascending=False)
+
+    print('=== agg_df')
+    # TODO Q1. Submit a screen shot of a batch ingestion of the aggregation
+    # TODO write output stream
+    query = agg_df \
+        .writeStream \
+        .format('console') \
+        .outputMode('Complete') \
+        .trigger(processingTime="10 seconds") \
+        .start()
+
+    # TODO attach a ProgressReporter
+    print('=== awaitTermination')
+    query.awaitTermination()
+
+    print('=== radio_code....')
+
+    # get the right radio code json path
+    radio_code_json_filepath = "radio_code.json"
+    radio_code_df = spark.read.json(radio_code_json_filepath)
+
+    # rename disposition_code column to disposition
+    radio_code_df = radio_code_df.withColumnRenamed("disposition_code", "disposition").collect()
+
+    # join on disposition column
+    join_query = agg_df.join(radio_code_df, col("agg_df.disposition") == col("radio_code_df.disposition"), "left_outer")
+
+    join_query.awaitTermination()
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+
+    # Create Spark in Standalone mode
+    spark = SparkSession \
+        .builder \
+        .config("spark.ui.port", 3000) \
+        .master("local[*]") \
+        .appName("KafkaSparkStructuredStreaming") \
+        .getOrCreate()
+
+    logger.info("Spark started")
+
+    run_spark_job(spark)
+
+    logger.info("Spark stoped")
+    spark.stop()
